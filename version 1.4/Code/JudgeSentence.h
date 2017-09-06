18332d163854d58eec7b7a5f60c726e8c8d59cd4
QisFj <813011968@qq.com>
2017-09-06 12:31:07 +0800

    add ,break,continue

---
+++ version 1.4/Code/JudgeSentence.h
@@ -0,0 +1,23 @@
+//
+// Created by dudu on 9/4/17.
+//
+
+#ifndef CLIONPROJECT_JUDGESENTENCE_H
+#define CLIONPROJECT_JUDGESENTENCE_H
+#include "PartWord.h"
+bool judgeExpression(vector<worder>& words, const pair<int, int> & p);
+bool judgeExpression(vector<worder>& words);
+bool expCheck(vector<worder>& words,const pair<int,int>& p);
+bool judgeMain(vector<worder> worders);
+int judgeFunction(const vector<worder>& worders);
+int judgeIfElseWhileReturn(vector<worder> worders);
+int judgeInAndOut(vector<worder> worders);
+bool judgeDeclartion(vector<worder> worders);
+bool judgeAssign(vector<worder> worders);
+bool judgeSingleExp(vector<worder> worders);
+bool judgeSingleRightBracket(vector<worder> worders);
+bool judgeOut(vector<worder> worders);
+
+
+
+#endif //CLIONPROJECT_JUDGESENTENCE_H
